611328f041bc4f765c459acead8484f5b60611d5
Sharon Konki <sharonkonki@gmail.com>
2022-03-11 10:21:24 -0600

    2nd commit - completed part 2_persisting employers & skills

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required.")
+    @Size(min = 3, max = 25, message = "Location should be between 3 & 25 characters long.")
+    public String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
